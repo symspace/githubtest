4b6fc9374d41d89152c039f24f32a97746ca20ab
Zayden <zayden.aputon@gmail.com>
2023-11-18 17:28:56 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by user on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
